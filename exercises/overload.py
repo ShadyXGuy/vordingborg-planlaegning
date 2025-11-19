9fd8702f6f69872ad8d5dbd758cb959d3e4f8e30
ShadyXGuy <156676283+ShadyXGuy@users.noreply.github.com>
2025-11-19 10:16:50 +0100

    Øvelser, nåde ikke de sidste i polymorph.py

--- exercises/overload.py
+++ exercises/overload.py
@@ -1,3 +1,14 @@
+#Udkommenter koden der implementerer __add__ og forklar fejlbeskeden ved kørsel.
+#Hvilken anden speciel metode er implementeret for klassen Point? Hvad bruges den til i kodeeksemplet?
+#Udvid koden med overlæsning af subtraktion og/eller negativt fortegn, se operator module.
+#Lav en generisk funktion til at udskrive "Simon says hello" hvor "hello"
+#skal være et funktionsargument af en type der implementerer metoden __str__.
+#Afprøv både med et Point, et tal og en tekststreng.
+from random import randint
+
+from numpy.random import random_integers
+
+
 class Point:
     def __init__(self, x, y):
         self.x = x
@@ -9,7 +20,23 @@ class Point:
     def __str__(self):
         return f"({self.x}, {self.y})"
 
+    def __sub__(self, other):
+        return Point(self.x - other.x, self.y - other.y)
+
+
+
 
 a = Point(1, 0)
 b = Point(2, 3)
-print(a + b)
+
+print (a - b)
+
+print (a + b)
+
+
+def simonSay(say):
+    print(f"Simon says {say}")
+
+simonSay("hello") # say: = str
+simonSay(2) # say: = int
+simonSay(a) # say: = Point
--- exercises/polymorph.py
+++ exercises/polymorph.py
@@ -1,3 +1,24 @@
+#Brug polymorph_gui.py som udgangspunkt (kræver pygame).
+
+#Læs koden og kør den. Forklar (skriv fx ned til logbog) og hvilken klasse der er defineret
+#og hvad den nedarver fra.
+
+#Fjern variablerne house1 og house2 og lav i stedet en liste med min. 3 huse.
+#Tegn dem alle vha. en løkke.
+
+#Lav din egen klasse til en simpel geometrisk tegning fx Z til at tegne Zorros tegn.
+#Den skal nedarve fra samme klasse som husene.
+
+#Lav en sprite.Group med både huse og dine egne objekter. Tegn dem alle vha. et kald til draw() på gruppen.
+
+#Lav en underklasse til Sprite kaldet Shape.
+
+    #Tilføj en metode i klassen med signaturen def get_points(self) som skal returnere alle punkter i figuren.
+
+    #Opdater klassen for huse og din egen metode til at nedarve fra Shape i stedet for Sprite.
+
+    #Sørg for at metoden get_points kun er defineret i Shape.
+
 import pygame
 
 
@@ -17,12 +38,15 @@ class House(pygame.sprite.Sprite):
         self.rect = self.rect.move(x - 25, y)
 
 
+
+
 pygame.init()
 screen = pygame.display.set_mode((500, 500))
 
-house1 = pygame.sprite.Group(House(50, 100))
-house2 = pygame.sprite.Group(House(250, 100))
+#house1 = pygame.sprite.Group(House(50, 100))
+#house2 = pygame.sprite.Group(House(250, 100))
 
+hus = [pygame.sprite.Group(House(250, 100)), pygame.sprite.Group(House(50, 100)), pygame.sprite.Group(House(450, 100))]
 
 def run():
     while True:
@@ -31,8 +55,10 @@ def run():
                 return
 
         screen.fill((255, 255, 255))
-        house1.draw(screen)
-        house2.draw(screen)
+        for i in hus:
+            i.draw(screen)
+        #house1.draw(screen)
+        #house2.draw(screen)
         pygame.display.flip()
 
 
